da2e3280af8734996ba3827c0ecb635c126dff16
Tolegen Timur <demigodkz@gmail.com>
2022-09-29 04:42:04 +0600

    feat: add Routes component

---
+++ src/app/ui/Routes.tsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import { Routes, Route, HashRouter as Router, Navigate } from 'react-router-dom';
+
+import MainPage from 'pages/MainPage';
+import AboutPage from 'pages/AboutPage';
+import PageNotFound from 'pages/PageNotFound';
+import DrawerLayout from 'widgets/DrawerLayout';
+import { ROUTE_PATH } from 'shared/common/constants';
+import 'app/lib/style.scss';
+
+class MainRoutes extends Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path={ROUTE_PATH.INDEX} element={<DrawerLayout />}>
+            <Route index element={<MainPage />} />
+            <Route path={ROUTE_PATH.ABOUT_US} element={<AboutPage />} />
+            <Route path={ROUTE_PATH.NOT_FOUND} element={<PageNotFound />} />
+            <Route path="*" element={<Navigate to={ROUTE_PATH.NOT_FOUND} replace />} />
+          </Route>
+        </Routes>
+      </Router>
+    );
+  }
+}
+
+export default MainRoutes;
